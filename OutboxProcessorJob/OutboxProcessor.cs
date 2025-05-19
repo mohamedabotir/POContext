a1599a2b76a1ca2d73bbd22800faa0eaea3951c5
mohamedabotir <mohamedabotyear@gmail.com>
2025-05-19 22:51:28 +0300

    use retry policy

--- OutboxProcessorJob/OutboxProcessor.cs
+++ OutboxProcessorJob/OutboxProcessor.cs
@@ -12,39 +12,46 @@ public class OutboxProcessor
     private readonly string _topic;
     private readonly AsyncRetryPolicy _retryPolicy;
 
-
     public OutboxProcessor(IEventRepository eventRepo, IProducer producer, IOptions<Topic> options)
     {
         _eventRepo = eventRepo;
         _producer = producer;
         _topic = options.Value.TopicName;
+
         _retryPolicy = Policy
-    .Handle<Exception>()
-    .WaitAndRetryAsync(
-        retryCount: 3,
-        sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)),
-        onRetry: (exception, timeSpan, retryCount, context) =>
-        {
-            Log.Information($"🔁 Retry {retryCount} after {timeSpan.TotalSeconds}s due to: {exception.Message}");
-        });
+            .Handle<Exception>()
+            .WaitAndRetryAsync(
+                retryCount: 3,
+                sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)),
+                onRetry: (exception, timeSpan, retryCount, context) =>
+                {
+                    Log.Warning($"🔁 Retry {retryCount} for event {context["EventId"]} after {timeSpan.TotalSeconds}s: {exception.Message}");
+                });
     }
 
     public async Task RunAsync(CancellationToken cancellationToken)
     {
         var unprocessed = await _eventRepo.GetUnprocessedEventsAsync();
+
         foreach (var evt in unprocessed)
         {
+            var context = new Context();
+            context["EventId"] = evt.Id.ToString();
+
             try
             {
-                await _producer.ProduceAsync(_topic, evt.EventBaseData);
-                await _eventRepo.MarkAsProcessed(evt.Id); 
+                await _retryPolicy.ExecuteAsync(async ctx =>
+                {
+                    await _producer.ProduceAsync(_topic, evt.EventBaseData);
+                    await _eventRepo.MarkAsProcessed(evt.Id);
+                }, context);
             }
             catch (Exception ex)
             {
-                Log.Error($"❌ Failed to process event {evt.Id}: {ex.Message}");
+                Log.Error($"❌ Failed after 3 retries for event {evt.Id}: {ex.Message}");
             }
         }
 
-        Log.Error("✅ Finished processing outbox events.");
+        Log.Information("✅ Finished processing outbox events.");
     }
 }
